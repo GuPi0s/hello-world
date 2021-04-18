4cfab1b9547f1c31a4f5b1c84d849c64bcc38b3b
GuPi0s <oscarg.gutierrezp@gmail.com>
2021-04-18 06:31:02 -0400

    Conmit changes.

--- canvas/document-draw.js
+++ canvas/document-draw.js
@@ -1,7 +1,10 @@
 var numLine = document.getElementById("numIn");
 var start = document.getElementById("buttom_start");
 var color = document.getElementById("color");
+var set_graph = document.getElementById("set_graph");
+var graphForm = document.getElementById("form");
 start.addEventListener("click", drawByClick );
+set_graph.addEventListener("click", setGraph );
 
 var d = document.getElementById("canvas");
 var lienzo = d.getContext("2d");
@@ -15,15 +18,31 @@ function drawByClick() {
   var espacio = ancho / linea;
   var l = 0;
   var xi, nxi;
-  var lineColor = color.value;
 
+  if (graph == "line") {
   for(l = 0; l < linea; l++) {
     xi = espacio * l;
     nxi = linea - espacio * l;
-    drawLine(lineColor, 0, xi, xi, 300);
-    drawLine(lineColor, xi, 0, 300, xi);
+    drawLine(color, 0, xi, xi, 300);
+    drawLine(color, xi, 0, 300, xi);
     console.log("linea " + l);
   }
+  }
+  else if(graph == "fullLine") {
+    for(l = 0; l < linea; l++) {
+      xi = espacio * l;
+      nxi = linea - espacio * l;
+      drawLine(color, 0, xi, xi, 300);
+      drawLine(color, 0, nxi, xi, 0);
+      drawLine(color, xi, 0, 300, xi);
+      drawLine(color, xi, 300, 300, nxi);
+      console.log("linea " + l);
+    }
+  }
+  else {
+    alert("Por favor elige un tipo de gráfico.");
+  }
+
   drawLine(color, 1, 1, anc, 1);
   drawLine(color, 1, 1, 1, alt);
   drawLine(color, 1, alt, anc, alt);
@@ -37,4 +56,13 @@ function drawLine(color, x_start, y_start, x_final, y_final) {
   lienzo.lineTo(x_final, y_final);
   lienzo.stroke();
   lienzo.closePath();
+}
+
+function drawRect(color, g, b, x_start, y_start, x_size, y_size) {
+  lienzo.fillStyle = color;
+  lienzo.fillRect(x_start, y_start, x_size, y_size);
+}
+
+function setGraph() {
+  var graph = graphForm;
 }
